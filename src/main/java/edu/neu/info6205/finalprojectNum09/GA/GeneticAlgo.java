d976a5fbe0281243ed29d71fac065bec302209e5
Alfred Koo <gu.ju@husky.neu.edu>
2017-12-07 14:36:29 -0500

    12.7 undone

---
+++ src/main/java/edu/neu/info6205/finalprojectNum09/GA/GeneticAlgo.java
@@ -0,0 +1,34 @@
+package edu.neu.info6205.finalprojectNum09.GA;
+
+/**
+ * @author  Junyuan Gu
+ * @NUID    001825583
+ */
+public class GeneticAlgo {
+    private Gene[] gene = null;
+    private StringBuffer parentGene = null;
+    private StringBuffer childGene = null;
+
+
+    /**
+     * default constructor
+     */
+    public GeneticAlgo() {
+
+
+    }
+
+    public void selection() {
+
+    }
+
+    public void crossover() {
+
+    }
+
+    public void mutation() {
+
+    }
+
+
+}
